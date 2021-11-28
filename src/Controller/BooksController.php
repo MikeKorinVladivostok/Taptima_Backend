174a8a91d2a8470362325a96e18b2714b4635666
mike <KorinetskiyMikhail@yandex.ru>
2021-11-29 09:57:37 +1000

    return to native PHP

--- src/Controller/BooksController.php
+++ src/Controller/BooksController.php
@@ -7,31 +7,37 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+
 class BooksController extends AbstractController
 {
     /**
      * @Route("/books", name="books")
      */
-    public function writeData() {
-//        $entityManager = $this->getDoctrine()->getManager();
-//
-//        $book = new Books();
-//
-//        $book -> setName("Земной круг");
-//        $book -> setAuthor("Abicrombey");
-//        $book -> setTitle("Темное фантази");
-//        $book -> setImage("some href...");
-//        $book -> setYear(2007);
-//
-//        $entityManager->persist($book);
-//        $entityManager->flush();
 
-        return $this->render('crud.html', [
-//            'id' => $book->getId(),
-        ]);
+
+    public function writeData() : Void {
+
+        $entityManager = $this->getDoctrine()->getManager();
+
+        $book = new Books();
+
+        $book -> setName("Земной круг");
+        $book -> setAuthor("Abicrombey");
+        $book -> setTitle("Темное фантази");
+        $book -> setImage("some href...");
+        $book -> setYear(2007);
+
+        $entityManager->persist($book);
+        $entityManager->flush();
+
     }
 
-//    public function readData() {
-//
-//    }
+    public function readData() {
+
+        $book = new Books();
+
+        return $this->render('crud.html', [
+            'id' => $book->getId(),
+        ]);
+    }
 }
