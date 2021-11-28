3e79452b6315c34fb0a36dfb121555a0495c6689
mike <KorinetskiyMikhail@yandex.ru>
2021-11-28 17:03:17 +1000

    make migration, test DB write data, create new correct controller

---
+++ src/Controller/BooksController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Books;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class BooksController extends AbstractController
+{
+    /**
+     * @Route("/books", name="books")
+     */
+    public function index(): Response
+    {
+        $number = "hi";
+        return $this->render('crud.html', [
+            'number' => $number,
+        ]);
+    }
+    public function writeData() {
+        $entityManager = $this->getDoctrine()->getManager();
+
+        $book = new Books();
+
+        $book -> setName("Земной круг");
+        $book -> setAuthor("Abicrombey");
+        $book -> setTitle("Темное фантази");
+        $book -> setImage("some href...");
+        $book -> setYear(2007);
+
+        $entityManager->persist($book);
+        $entityManager->flush();
+
+        return $this->render('crud.html', [
+            'id' => $book->getId(),
+        ]);
+    }
+}
